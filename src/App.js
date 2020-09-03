bba1795ba596b21c9b539a99de9836d70ed952ca
RonaldoFederighi <ronaldo.federighi@gmail.com>
2020-09-03 02:38:04 -0300

    Limpando o Projeto
    Foi feito uma limpeza eliminando muitos elmentos da pagina inicial.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o arquivo <code>src/App.js</code>, salve e recarregue esta pagina.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
-      </header>
-    </div>
+    <h1>Ola Mundo</h1>
   );
 }
 
